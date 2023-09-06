4ee95258f98687b485326971e6d95c3026e154fd
moonhee <zipsy327@gmail.com>
2023-09-06 17:17:26 +0900

    array

--- javawork/NaverJavaProject/src/bit701/day0906/Ex13_ArraySort.java
+++ javawork/NaverJavaProject/src/bit701/day0906/Ex13_ArraySort.java
@@ -8,7 +8,7 @@ public class Ex13_ArraySort {
 		//selection sort
 		//오름차순 정렬 : 기준(i)이 비교되는 값(j)보다 클경우 바꾼다
 		for(int i=0;i<data.length-1;i++)
-		{
+		{  
 			for(int j=i+1;j<data.length;j++)
 			{
 				//if(data[i]>data[j])//오름차순 정렬
