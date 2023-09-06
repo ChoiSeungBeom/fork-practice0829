f62bfa8a4b96f3a513850a0328a687b03645cfc0
moonhee <zipsy327@gmail.com>
2023-09-06 17:38:43 +0900

    hello

--- javawork/NaverJavaProject/src/bit701/day0906/Hello.java
+++ javawork/NaverJavaProject/src/bit701/day0906/Hello.java
@@ -1,10 +1,10 @@
 package bit701.day0906;
 
-public class Test {
+public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test111222333");
+		System.out.println("Hello");
 	}
 
 }
