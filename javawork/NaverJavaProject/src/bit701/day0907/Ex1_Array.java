12b2ebb042a0207bb2ab51dd5030521849acd3f0
moonhee <zipsy327@gmail.com>
2023-09-07 12:06:47 +0900

    빙고게임

--- javawork/NaverJavaProject/src/bit701/day0907/Ex1_Array.java
+++ javawork/NaverJavaProject/src/bit701/day0907/Ex1_Array.java
@@ -1,11 +1,77 @@
 package bit701.day0907;
 
+import java.util.Scanner;
+
 public class Ex1_Array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("배열 복습 예제");
+		/*
+		 *  처음에 인원수(count)를 입력후 그 인원수만큼
+		 *  names, kor,eng 를 배열 할당한후
+		 *  인원수만큼 이름,국어점수,영어점수를 입력하면
+		 *  
+		 *  아래와 같이 출력되도록 프로그램을 작성하세요
+		 *  
+		 *  번호  이름  국어  영어  총점   평균
+		 *  ----------------------------------
+		 *  1     김     89    90    179   89.5
+		 *  2     이     100  100    200  100
+		 */
+		Scanner sc=new Scanner(System.in);
+		int count;
+		String []names;
+		int []kor,eng,tot;
+		double []avg;
+		
+		System.out.println("입력할 인원수는?");
+		count=Integer.parseInt(sc.nextLine());
+		//배열 할당
+		names=new String[count];
+		kor=new int[count];
+		eng=new int[count];
+		tot=new int[count];
+		avg=new double[count];
+		
+		//데이타 입력
+		for(int i=0;i<count;i++)
+		{
+			System.out.println(i+"번 이름");
+			names[i]=sc.nextLine();
+			
+			System.out.println(i+"번 국어,영어점수");
+			kor[i]=Integer.parseInt(sc.nextLine());
+			eng[i]=Integer.parseInt(sc.nextLine());
+			
+			//총점 , 평균 계산
+			tot[i]=kor[i]+eng[i];
+			avg[i]=tot[i]/2.0;
+			System.out.println();
+		}		
+		
+		//출력
+		System.out.println("번호\t이름\t국어\t영어\t총점\t평균");
+		System.out.println("=".repeat(40));
+		for(int i=0;i<count;i++)
+		{
+			System.out.println(i+1+"\t"+names[i]+"\t"+kor[i]+"\t"+
+					eng[i]+"\t"+tot[i]+"\t"+avg[i]);
+		}
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javawork/NaverJavaProject/src/bit701/day0907/Ex2_Array2.java
@@ -0,0 +1,46 @@
+package bit701.day0907;
+
+public class Ex2_Array2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//2차원 배열 선언 방법
+		int [][]arr=new int[2][3];
+		System.out.println("행갯수:"+arr.length);
+		System.out.println("0번행의 열갯수:"+arr[0].length);
+		System.out.println("1번행의 열갯수:"+arr[1].length);
+		
+		arr[0][1]=3;
+		arr[1][1]=5;
+		System.out.println("첫번째 배열 출력");
+		arrayWrite(arr);
+		
+		//2번째 방법
+		int [][]arr2= {
+				{1,2},{3,4,5},{10,20,30,40}
+		};//0번행 열갯수:2 , 1번행 열갯수:3...
+		
+		System.out.println("** 2번째 배열 출력 **");
+		arrayWrite(arr2);
+		
+		//3번째 방법
+		int [][]arr3;
+		arr3=new int[][] {{1,2},{1,2,3},{4,5,6,7}};
+		System.out.println("** 3번째 배열 출력 **");
+		arrayWrite(arr3);
+	}
+	
+	public static void arrayWrite(int [][]arr)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				System.out.printf("[%2d][%2d]=%2d ",i,j,arr[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("=".repeat(40));
+	}
+
+}
