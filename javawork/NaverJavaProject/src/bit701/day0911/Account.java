3bf3d9cbfd2bfa99a83337e0c6965dc85d86db85
moonhee <zipsy327@gmail.com>
2023-09-11 16:28:33 +0900

    account교재예제

---
+++ javawork/NaverJavaProject/src/bit701/day0911/Account.java
@@ -0,0 +1,19 @@
+package bit701.day0911;
+//교재 클래스부분 20번 문제
+public class Account {
+  private String accountNo;//계좌번호
+  private String accountName;//계좌주
+  private int money;//잔액
+  
+  //3개의 값을 전달받는 생성자
+  
+  //계좌번호가 맞을경우 true 를 반환해주는 메서드
+  //    isAccount(String accountNo) : 맞을경우 true 반환,틀릴경우 false반환
+  
+  //외부에서 입금시 money 에 추가하는 메서드(addMoney)
+	  
+  //외부에서 출금시 money 에서 빼는 메서드(subMoney)
+  
+  //출력해주는 메서드   계좌번호  예금주  잔액(accountWrite)-+  
+  
+}
---
+++ javawork/NaverJavaProject/src/bit701/day0911/Book_BankApplication.java
@@ -0,0 +1,20 @@
+package bit701.day0911;
+
+import java.util.Scanner;
+
+//교재 클래스부분 20번 문제
+public class Book_BankApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Account []account=new Account[10];
+		Scanner sc=new Scanner(System.in);
+		//배열 10개가 꽉차면 "더이상 계좌생성이 안됩니다" 라고 출력
+		while(true)
+		{
+			
+		}	
+
+	}
+
+}
